1a4e37cd1f6fe3ff37c9ddb5fd84ca48ed2ee2f1
Arthur C <arthurcw@gmail.com>
2020-01-26 20:34:33 -0600

    completed bonus

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -19,7 +19,7 @@ DELIMITER ',' CSV HEADER;
 -- 2) create employee table and import data
 CREATE TABLE employees (
 	emp_no INT NOT NULL PRIMARY KEY,
-	birth_date DATE NOT NULL,
+	birth_date DATE,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR(1) NOT NULL,
@@ -31,11 +31,14 @@ FROM 'C:/Users/chanwc/Documents/NU_DS_HW/HW7_SQL/data/employees.csv'
 DELIMITER ',' CSV HEADER;
 
 -- 3) create department employee table and import data
+-- from_date also selected as primary key so an employee may switch 
+-- departments then back to same one in future
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, dept_no, from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
@@ -45,11 +48,15 @@ FROM 'C:/Users/chanwc/Documents/NU_DS_HW/HW7_SQL/data/dept_emp.csv'
 DELIMITER ',' CSV HEADER;
 
 -- 4) create department manager table and import data
+-- dept # and from date as primary keys assuming one manager per
+-- department at a time; employee # not selected as the same
+-- employee may become department manager multiple times
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
+	PRIMARY KEY (dept_no, from_date),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -60,10 +67,11 @@ DELIMITER ',' CSV HEADER;
 
 -- 5) create salaries table and import data
 CREATE TABLE salaries (
-	emp_no INT NOT NULL PRIMARY KEY,
+	emp_no INT NOT NULL,
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
@@ -72,11 +80,14 @@ FROM 'C:/Users/chanwc/Documents/NU_DS_HW/HW7_SQL/data/salaries.csv'
 DELIMITER ',' CSV HEADER;
 
 -- 6) create titles table and import data
+-- from_date also selected as primary key so an employee may switch 
+-- positions then back to same title in future
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
+	PRIMARY KEY (emp_no, title, from_date),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # SQL Homework
 ## Database Diagram
 ![ImageERD](https://github.com/arthurcw/sql-challenge/blob/master/data/Database_Diagram.png)
+
+## Salary
+![ImageSalary](https://github.com/arthurcw/sql-challenge/blob/master/python/salary_histogram.png)
+
+## Average Salary by Title
+![ImageSalary](https://github.com/arthurcw/sql-challenge/blob/master/python/SalaryByTitle.png)
